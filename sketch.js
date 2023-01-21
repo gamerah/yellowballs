f3f914ed626eccac257f953d1c6e4d798a0ba738
Dani Alias <danielgutierrezalias@gmail.com>
2023-01-22 00:47:35 +0900

    Responsive centered
    Now the subcanvas is centered in the window.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
 // Tiny Yellow Balls
 // A Gamerah Project
 
-var offsetX, offsetY, windowMinSide; // Responsive canvas setup
+// `offsetX`, `offsetY`: the offset where our 9x16 "subcanvas" will be drawn
+// `subW`, `subH`: dimensions of our "subcanvas"
+// (not really a canvas itself, just the area we draw onto)
+var offsetX, offsetY, subW, subH;
+
 var current_wave = 1;
 var score = 0;
 var lives = 3;
@@ -18,8 +22,11 @@ var r = 255,
   colorsign = +1;
 
 function setup() {
-  // Canvas
-  createCanvas(displayWidth, displayHeight);
+  // The canvas should cover the whole viewport
+  createCanvas(windowWidth, windowHeight);
+  // `windowResized` is automatically called by p5.js on window resized events,
+  // but we call it here manually to initialize the offset and dimensions
+  // of the area we will be drawing onto
   windowResized();
 
   // Ball setup
@@ -29,35 +36,15 @@ function setup() {
   noStroke();
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  windowMinSide = min(windowHeight, windowWidth);
-  offsetX = (windowWidth - windowMinSide) / 2;
-  offsetY = (windowHeight - windowMinSide) / 2;
-
-  // Setting the display height and width based on the window height
-  displayHeight = windowHeight;
-  displayWidth = windowHeight * 0.5625; // Ratio 9:16
-}
-
-// Fullscreen code taken from @takawo
-// Instead mousePressed, let's create a button for this
-/*function mousePressed() {
-  let fs = fullscreen();
-  fullscreen(!fs);
-  if (fs) {
-      cursor();
-  } else {
-      noCursor();
-  }
-}*/
-
 function draw() {
   background(192);
+  // `push` remembers the current `translate` offsets as a checkpoint
+  // Offsets are currently 0, 0 since we have not called translate yet
   push();
-  //translate(offsetX, offsetY);
-  rect(0, 0, displayWidth, displayHeight); // red rectangle
-  pop();
+  // Everything we draw after `translate` will be added the given offsets
+  translate(offsetX, offsetY);
+  // Draw a white rectangle covering the 9x16 area
+  rect(0, 0, subW, subH);
 
   // console.log("balls:" + ball.length + " max_balls_in_wave:" + max_balls_in_wave + ' since last_spawn:' + (millis() - last_spawn));
   if (ball.length < max_balls_in_wave && millis() - last_spawn > spawn_period) {
@@ -80,24 +67,14 @@ function draw() {
   }
 
   fill(r, g, b);
-  ellipse(
-    displayWidth / 5,
-    displayHeight / 12,
-    displayWidth / 5,
-    displayWidth / 5
-  );
+  ellipse(subW / 5, subH / 12, subW / 5, subW / 5);
   if (g > 100 || g < 0) {
     colorsign = -colorsign;
   }
   g = g + colorsign;
   b = b + colorsign;
   fill(0);
-  ellipse(
-    displayWidth / 2,
-    displayHeight * 4,
-    displayHeight * 6.8,
-    displayHeight * 6.8
-  );
+  ellipse(subW / 2, subH * 4, subH * 6.8, subH * 6.8);
   if (lives > 0) {
     txoropito1.move();
     txoropito2.move();
@@ -123,12 +100,12 @@ function draw() {
     }
   }
   for (var i = ball.length - 1; i >= 0; i--) {
-    if (ball[i].y > displayHeight) {
+    if (ball[i].y > subH) {
       ball.splice(i, 1);
     }
   }
   fill(0);
-  text(score, displayWidth / 20, displayHeight / 12);
+  text(score, subW / 20, subH / 12);
   var lives_cache = lives;
   var lives_str = "";
   while (lives_cache > 0) {
@@ -136,18 +113,25 @@ function draw() {
     lives_cache = lives_cache - 1;
   }
   fill(255, 0, 0);
-  text(lives_str, displayWidth - displayWidth / 8, displayHeight / 12);
+  text(lives_str, subW - subW / 8, subH / 12);
   if (lives < 1) {
-    text("UR REKT", displayWidth - displayWidth / 6, displayHeight / 12);
+    text("UR REKT", subW - subW / 6, subH / 12);
   }
+
+  // Revert the effects of `translate` by going back to the previous offset checkpoint
+  pop();
 }
 
 function txoropito(sign) {
-  this.x = displayWidth / 2;
-  this.y = displayHeight - displayHeight / 6;
+  push();
+  // Everything we draw after `translate` will be added the given offsets
+  translate(offsetX, offsetY);
+
+  this.x = subW / 2;
+  this.y = subH - subH / 6;
   this.color = color(100, 200, 255);
-  this.diameter = displayWidth / 16;
-  this.speed = (sign * displayWidth) / 128;
+  this.diameter = subW / 16;
+  this.speed = (sign * subW) / 128;
 
   this.move = function () {
     if (mouseIsPressed || keyIsPressed) {
@@ -155,16 +139,13 @@ function txoropito(sign) {
     } else {
       this.x = this.x - this.speed;
     }
-    if (this.speed > 0 && this.x < displayWidth / 2 + this.diameter / 2) {
-      this.x = displayWidth / 2 + this.diameter / 2;
-    } else if (
-      this.speed < 0 &&
-      this.x > displayWidth / 2 - this.diameter / 2
-    ) {
-      this.x = displayWidth / 2 - this.diameter / 2;
+    if (this.speed > 0 && this.x < subW / 2 + this.diameter / 2) {
+      this.x = subW / 2 + this.diameter / 2;
+    } else if (this.speed < 0 && this.x > subW / 2 - this.diameter / 2) {
+      this.x = subW / 2 - this.diameter / 2;
     }
-    if (this.x > displayWidth - this.diameter / 2) {
-      this.x = displayWidth - this.diameter / 2;
+    if (this.x > subW - this.diameter / 2) {
+      this.x = subW - this.diameter / 2;
     } else if (this.x < this.diameter / 2) {
       this.x = this.diameter / 2;
     }
@@ -179,19 +160,25 @@ function txoropito(sign) {
       this.diameter
     );
   };
+
+  // Revert the effects of `translate` by going back to the previous offset checkpoint
+  pop();
 }
 
 function yellowBall(sign, span) {
+  push();
+  // Everything we draw after `translate` will be added the given offsets
+  translate(offsetX, offsetY);
+
   this.was_hit = false;
   this.was_scored = false;
-  this.diameter = displayWidth / 20;
+  this.diameter = subW / 20;
   this.color = color(255, 204, 0);
-  this.x = displayWidth / 5;
-  this.y = displayHeight / 12;
+  this.x = subW / 5;
+  this.y = subH / 12;
   var framestofall = 300;
-  this.yspeed =
-    (displayHeight * (1 - this.y / displayHeight - 1 / 6)) / framestofall;
-  var destination = (displayWidth * (1 + sign * span)) / 2;
+  this.yspeed = (subH * (1 - this.y / subH - 1 / 6)) / framestofall;
+  var destination = (subW * (1 + sign * span)) / 2;
   this.xspeed = (destination - this.x) / framestofall;
   // console.log('span:' + sign * span + ' destination:' + destination + ' xspeed:' + this.xspeed);
 
@@ -216,4 +203,38 @@ function yellowBall(sign, span) {
     fill(this.color);
     ellipse(this.x, this.y, this.diameter, this.diameter);
   };
+
+  // Revert the effects of `translate` by going back to the previous offset checkpoint
+  pop();
+}
+
+function windowResized() {
+  // We want to resize the whole p5.js canvas
+  // to make sure it always is the same size as the viewport
+  resizeCanvas(windowWidth, windowHeight);
+  // This is the aspect ratio of the viewport,
+  // which will usually be bigger (wider) or smaller (taller) than 9:16
+  const windowAspect = windowWidth / windowHeight;
+  // console.log(
+  //   "windowAspect",
+  //   windowAspect,
+  //   (windowAspect * 16).toFixed(2) + ":16"
+  // );
+  if (windowAspect > 9 / 16) {
+    // If we have a wide viewport
+    // console.log("margin on the sides");
+    // We match the viewport's height
+    subH = windowHeight;
+    // and calculate the width that will result in 9:16
+    subW = (windowHeight * 9) / 16;
+  } else {
+    // If we have a tall viewport
+    // console.log("margin on top and bottom");
+    subW = windowWidth;
+    subH = (windowWidth * 16) / 9;
+  }
+  // console.log("canvas", windowWidth, windowHeight, "subW subH", subW, subH);
+  // Initialize the offsets for `translate`
+  offsetX = (windowWidth - subW) / 2;
+  offsetY = (windowHeight - subH) / 2;
 }
